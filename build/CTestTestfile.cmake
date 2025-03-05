9504e4827bd1f8e5eee3a767db79efca828ee386
Bui Khanh Van <van.buikhanh@hotmail.com>
2025-03-05 14:10:28 +0000

    Remove nessessary part + update key generation string

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vanbu/Desktop/dilithium-pi5
-# Build directory: /home/vanbu/Desktop/dilithium-pi5/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[TestVectorFormatting]=] "/usr/bin/cmake" "-P" "/home/vanbu/Desktop/dilithium-pi5/build/check_vector_output.cmake")
-set_tests_properties([=[TestVectorFormatting]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/vanbu/Desktop/dilithium-pi5/CMakeLists.txt;72;add_test;/home/vanbu/Desktop/dilithium-pi5/CMakeLists.txt;0;")
--- test/main.cpp
+++ test/main.cpp
@@ -1,27 +1,87 @@
-/**
- * @file main.cpp
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2025-02-10
- * 
- * @copyright Copyright (c) 2025
- * 
- */
-
-#include <vector>
-#include <algorithm>
-#include <iostream>
-#include "utils.h"
-
-extern "C" {
-  #include "fips202.h"
-}
-
-int main() {
-  // check for the function of the file is the main thing that we check over
-  // chekc on the main 
-  keccak_state state;
-  shake128_init(&state);
-  
-}
+ #include <vector>
+ #include <algorithm>
+ #include <iostream>
+ #include <fstream>
+ #include <iomanip>
+ #include <sstream>
+ #include <string>
+ #include <cstring>
+ #include "utils.h"
+ #include "mldsa.h"
+ 
+ // Convert binary data to hex string
+ std::string to_hex_string(const uint8_t* data, size_t length) {
+     std::stringstream ss;
+     ss << std::hex << std::setfill('0');
+     
+     for (size_t i = 0; i < length; i++) {
+         ss << std::setw(2) << static_cast<int>(data[i]);
+     }
+     
+     return ss.str();
+ }
+ 
+ void print_usage() {
+     std::cout << "Usage: mldsa [OPTIONS]\n\n"
+               << "Options:\n"
+               << "  -g, --keygen    Generation of  ML-DSA keypair\n"
+               << "  -h, --help      Help\n"
+               << std::endl;
+ }
+ 
+ void generate_keypair() {
+     std::cout << "Generating ML-DSA keypair..." << std::endl;
+     
+     // Create ML-DSA object and generate keys
+     MLDSA mldsa;
+     std::array<uint8_t, CRYPTO_PUBLICKEYBYTES> public_key;
+     std::array<uint8_t, CRYPTO_SECRETKEYBYTES> secret_key;
+     
+     mldsa.KeyGen(public_key, secret_key);
+     
+     // Convert the binary keys to hex strings
+     std::string hex_public = to_hex_string(public_key.data(), public_key.size());
+     std::string hex_private = to_hex_string(secret_key.data(), secret_key.size());
+     
+     // Output the keys
+     std::cout << "Public key size: " << public_key.size() << " bytes" << std::endl;
+     std::cout << "Private key size: " << secret_key.size() << " bytes" << std::endl;
+     
+     // Save the keys to files
+     std::ofstream public_file("mldsa_public.txt");
+     public_file << hex_public;
+     public_file.close();
+     
+     std::ofstream private_file("mldsa_private.txt");
+     private_file << hex_private;
+     private_file.close();
+     
+     std::cout << "Keys have been saved in hex format to 'mldsa_public.txt' and 'mldsa_private.txt'" << std::endl;
+ }
+ 
+ int main(int argc, char* argv[]) {
+     // If no arguments provided, show usage
+     if (argc < 2) {
+         print_usage();
+         return 1;
+     }
+     
+     // Process command-line arguments
+     for (int i = 1; i < argc; i++) {
+         if (strcmp(argv[i], "-g") == 0 || strcmp(argv[i], "--keygen") == 0) {
+             generate_keypair();
+             return 0;
+         }
+         else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+             print_usage();
+             return 0;
+         }
+         else {
+             std::cerr << "Unknown option: " << argv[i] << std::endl;
+             print_usage();
+             return 1;
+         }
+     }
+     
+     return 0;
+ }
